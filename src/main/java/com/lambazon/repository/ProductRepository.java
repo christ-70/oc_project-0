e13b6a05e98151b32725e82e306ef0724369dee2
F85549 <christophe.maurille@enedis.fr>
2018-06-08 10:42:04 +0200

    Addition on L24 & 25 of 2 news products "Apple & Samsung" and modification quantity and price on L20 for Echo Dot product.

--- src/main/java/com/lambazon/repository/ProductRepository.java
+++ src/main/java/com/lambazon/repository/ProductRepository.java
@@ -17,11 +17,14 @@ public class ProductRepository {
 	
 	private static void generateProductData() {
 		int id=0;
-		products.put(++id, new Product(id, 10, 92.50, "Echo Dot", "(2nd Generation) - Black"));
+		products.put(++id, new Product(id, 10, 90.50, "Echo Dot", "(2nd Generation) - Black")); //Quantity & price modification
 		products.put(++id, new Product(id, 20, 9.99, "Anker 3ft / 0.9m Nylon Braided", "Tangle-Free Micro USB Cable"));
 		products.put(++id, new Product(id, 30, 69.99, "JVC HAFX8R Headphone", "Riptidz, In-Ear"));
 		products.put(++id, new Product(id, 40, 32.50, "VTech CS6114 DECT 6.0", "Cordless Phone"));
 		products.put(++id, new Product(id, 50, 895.00, "NOKIA OEM BL-5J", "Cell Phone "));
+		products.put(++id, new Product(id, 60, 700.00, "SAMSUNG S7 EDGE", "Cell Phone ")); //Addition of new product Samsung
+		products.put(++id, new Product(id, 70, 800.00, "APPLE IPHONE X", "Cell Phone ")); //Addition of new product Apple
+				
 	}
 	
 	public List<Product> products() {
