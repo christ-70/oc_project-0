26a19fb0ce4860157a5a1807df60b7f6996a789a
F85549 <christophe.maurille@enedis.fr>
2018-06-08 13:39:22 +0200

    Added fonction price*quantity = TotalInventoryPrice (L69&70) for calculate total Price inventory and L9 for variable.

--- src/main/java/com/lambazon/domain/Product.java
+++ src/main/java/com/lambazon/domain/Product.java
@@ -6,6 +6,7 @@ public class Product {
 	private String name, description, details;
 	private int quantity;
 	private double price;
+	private double totalInventoryPrice; //private double totalInventoryPrice added
 
 	public Product(int id, int quantity, double price, String name, String description) {
 		setId(id);
@@ -65,6 +66,7 @@ public class Product {
 
 	public double getInventoryPrice() {
 		// TODO Auto-generated method stub
-		return 0.0;
+		totalInventoryPrice = price*quantity; //added fonction price*quantity
+		return totalInventoryPrice;
 	}
 }
